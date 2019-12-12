ce58d2b6aa268425bb362282b668408b45287e6c
serggey00.00 <serggey00.00@gmail.com>
2019-12-12 09:42:02 +0300

    update teory

--- Project/Checkers/Checkers/main.cpp
+++ Project/Checkers/Checkers/main.cpp
@@ -2,9 +2,11 @@
 #include <Game.hpp>
 
 int main(int argc, char* argv[]) {
-    Game game(argv[1], argv[2]);
-    game.calculateBestMove(argv[3], argv[4]);
-    game.makeBestMove();
-    game.saveGame();
+    if (argc >= 4) {
+        Game game(argv[1], argv[2]);
+        game.calculateBestMove(argv[3], argv[4]);
+        game.makeBestMove();
+        game.saveGame();
+    }
     return 0;
 }
